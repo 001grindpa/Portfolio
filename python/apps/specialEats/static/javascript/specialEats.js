ab4e4518757871f6f90343405a90be1cb8e89bcd
guzo <guzo@gmail.com>
2025-08-03 11:09:41 +0100

    updated the js code

--- python/apps/specialEats/static/javascript/specialEats.js
+++ python/apps/specialEats/static/javascript/specialEats.js
@@ -2,13 +2,9 @@ document.addEventListener('DOMContentLoaded', function(){
 	let buggerContainer = document.querySelector('.menucheck')
 	let hambugger = document.querySelector('#menuCheck');
 	let body = document.querySelector('body');
+	let main = document.querySelector('main');
 
-	hambugger.addEventListener('change', function(){
-		if (hambugger.checked) {
-			body.style.overflow = 'hidden';
-		}
-		else {
-			body.style.overflow = 'auto';
-		}
+	main.addEventListener('click', function(){
+		hambugger.checked = false;
 	})	
 })
