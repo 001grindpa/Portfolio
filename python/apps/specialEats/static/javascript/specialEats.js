0f7f10bdf3b4095caa0d4d5641ab71863c1638dd
Grindpa <francisanyanwuchu@gmail.com>
2025-08-03 18:54:12 +0100

    done with the landing page pop-up configuration

--- python/apps/specialEats/static/javascript/specialEats.js
+++ python/apps/specialEats/static/javascript/specialEats.js
@@ -3,8 +3,46 @@ document.addEventListener('DOMContentLoaded', function(){
 	let hambugger = document.querySelector('#menuCheck');
 	let body = document.querySelector('body');
 	let main = document.querySelector('main');
+    let order_now = document.querySelector('#check');
+    let background = document.querySelector('.access');
+    let section = document.querySelector('#check-2');
+    let section_background = document.querySelector('#section');
+    let innerOrder = document.querySelector('.innerAccess');
+    let innerSection = document.querySelector('.sectionForm');
+
+    // hero section pop-up configuration
 
 	main.addEventListener('click', function(){
 		hambugger.checked = false;
-	})	
+	});
+    background.addEventListener('click', function(event){
+        order_now.checked = false;
+        order_now.dispatchEvent(new Event('change'));
+    });
+    section_background.addEventListener('click', function(event){
+        section.checked = false;
+        section.dispatchEvent(new Event('change'));
+    });
+    order_now.addEventListener('change', function(){
+        if (order_now.checked) {
+            body.style.overflow = 'hidden';
+        }
+        else {
+            body.style.overflow = 'auto';
+        }
+    });
+    section.addEventListener('change', function(event){
+        if (section.checked) {
+            body.style.overflow = 'hidden';
+        }
+        else {
+            body.style.overflow = 'auto';
+        }
+    });
+    innerOrder.addEventListener('click', function(event){
+        event.stopPropagation();
+    });
+    innerSection.addEventListener('click', function(event){
+        event.stopPropagation();
+    })
 })
