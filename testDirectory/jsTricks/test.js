93bd91eed74b36640bf08ae2f07af574047990ca
guzo <guzo@gmail.com>
2025-09-09 07:14:07 +0100

    added back the main js file

---
+++ testDirectory/jsTricks/test.js
@@ -0,0 +1,78 @@
+document.addEventListener("DOMContentLoaded", function(){
+	let counterC = document.querySelector(".counterC");
+	let counter = counterC.querySelector(".counter");
+	let front = counterC.querySelector(".front");
+	let back = counterC.querySelector(".back");
+	let total = document.querySelector("#total");
+	let saveBtn = counterC.querySelector(".save");
+	let n = parseInt(counter.textContent);
+	let total2 = document.querySelector("#total2");
+
+	
+	
+	saveBtn.addEventListener("click", function(){
+		total.innerHTML = n;
+		for (let i = 0; i <= 0; i++){
+			let t = document.createElement("span");
+			t.textContent = `+${n} `;
+			total2.appendChild(t);
+		}
+		counter.innerHTML = 0;
+		n = 0;
+	});
+	
+	front.addEventListener("click", function(){
+		n++;
+		counter.innerHTML = n;
+	});
+
+	back.addEventListener("click", function(event){
+		if (n === 0){
+			event.preventDefault();
+		}
+		else {
+			n--;
+			counter.innerHTML = n;
+		}
+	});
+
+	// human-dog age ratio
+	let human = document.querySelector("#human");
+	let dogResult = document.querySelector("#dogResult");
+	
+	human.addEventListener("input", function(event){
+		let humanAge = human.value;
+		let dogAge = (humanAge * 7) / 1;
+
+		dogResult.innerHTML = `${dogAge} years`;
+	});
+
+	// calculator
+	let btn = document.querySelectorAll(".button");
+	let input = document.querySelector("#input");
+	let clear = document.querySelector(".clear");
+	let equal = document.querySelector(".equal");
+	let del = document.querySelector("#delete");
+
+	del.addEventListener("click", function(){
+		input.value = input.value.slice(0, -1);
+	});
+
+	clear.addEventListener("click", function(){
+		input.value = "";
+	});
+
+	equal.addEventListener("click", function(){
+		input.value = eval(input.value);
+	});
+	
+	for (let i = 0; i <= btn.length; i++){
+		btn[i].addEventListener("click", function(){
+			input.value += btn[i].textContent;
+		})
+	}
+	
+	// blackjack game
+
+	
+})
